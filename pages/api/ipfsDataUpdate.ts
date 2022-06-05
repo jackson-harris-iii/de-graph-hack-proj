68c851facd6a69ddd89fd8e8a15101dad39c2a4c
Jackson Harris III <jackson@spotx.games>
2022-06-04 19:00:54 -0700

    handle ipfs update issues

--- pages/api/ipfsDataUpdate.ts
+++ pages/api/ipfsDataUpdate.ts
@@ -1,12 +1,10 @@
-import { withIronSessionApiRoute } from "iron-session/next"
-import { NextApiRequest, NextApiResponse } from "next"
-import { ironOptions } from "./ironOptions"
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { ironOptions } from './ironOptions';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { method } = req
-  
-      res.send({ ok: true })
-  }
-}
+  const { method } = req;
+  res.send({ ok: true });
+};
 
-export default handler
+export default handler;
